500eaa709b312cb4949612bfe3f6f26226d338a3
Durgesh <durgesh1812b@gmail.com>
2025-10-05 22:57:58 +0530

    changed size of the componenet

--- app/page.js
+++ app/page.js
@@ -471,8 +471,6 @@ const Home = () => {
             </div>
 
 
-
-
             {/* Resources & Support */}
             <div className="space-y-4">
               <h3 className="text-lg font-semibold text-white">Resources</h3>
